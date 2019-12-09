cedca6df01d8b89b7599b17278da435984fb6806
svelezg <svelez.velezgarcia@gmail.com>
2019-12-09 16:52:40 -0500

    4-puppet_ssh_config.pp

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Using Puppet, create a Client configuration.
+include stdlib
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no'
+}
+
+
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton'
+}
+
