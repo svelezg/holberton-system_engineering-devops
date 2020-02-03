2a8e5e08cdc99ab3d83b1d050af4ba64c2dfbe66
svelezg <svelez.velezgarcia@gmail.com>
2020-02-03 16:59:22 -0500

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,33 +4,26 @@ Using a REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
 if __name__ == "__main__":
+    import json
     import requests
     import sys
 
     employee_id = sys.argv[1]
     base_url = 'https://jsonplaceholder.typicode.com/'
 
-    url = base_url + 'todos'
-    response = requests.get(url)
-    # print(response.json())
-
-    url = base_url + 'users/' + employee_id
-    response_1 = requests.get(url, 'name')
+    url1 = base_url + 'users/' + employee_id
+    response_1 = requests.get(url1, 'name')
 
     # name of the employee
     employee_name = response_1.json().get('name')
 
     # total number of tasks, which is the sum of completed and
     # non-completed tasks
-    url = base_url + 'todos'
-    response_2 = requests.get(url)
-
-    # number of completed tasks
+    url2 = base_url + 'todos/' + "?userId=" + sys.argv[1]
+    response_2 = requests.get(url2)
     task_done = len([task for task in response_2.json()
-                     if task.get('userId') is int(employee_id) and
-                     task.get('completed') is True])
-    task_total = len([task for task in response_2.json()
-                      if task.get('userId') is int(employee_id)])
+                     if task.get('completed') is True])
+    task_total = len([task for task in response_2.json()])
 
     print("Employee {} is done with tasks({}/{}):".
           format(employee_name, task_done, task_total))
