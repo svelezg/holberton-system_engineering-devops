f5cf1fd08c802db06def8508d86609aa7be6a027
svelezg <svelez.velezgarcia@gmail.com>
2020-02-06 14:49:26 -0500

    Task 100 case insensitive also in search word

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -29,20 +29,18 @@ def count_words(subreddit, word_list, hot_list_titles=[], after='null'):
     the following line"""
     # print(len(hot_list_titles))
 
-    # after = None
     if after is None:
-        to_print_dict = []
+        to_print_tuples = []
         for word in word_list:
             count = 0
             for title in hot_list_titles:
                 split_title = title.split()
                 new_split = [element.lower() for element in split_title]
-                count = count + new_split.count(word)
+                count = count + new_split.count(word.lower())
             if count != 0:
-                to_print_dict.append((word, count))
-        for elem in sorted(to_print_dict, key=lambda x: (-x[1], x[0])):
+                to_print_tuples.append((word, count))
+        for elem in sorted(to_print_tuples, key=lambda x: (-x[1], x[0])):
             print("{}: {}".format(elem[0], elem[1]))
-
     else:
         return count_words(subreddit, word_list,
                            hot_list_titles, after)
